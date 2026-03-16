5ad565adaffbcf1d960eae9f9cbaa6bb3603f895
h-el-lo <thisliny@gmail.com>
2026-03-16 17:22:48 +0100

    Correctly compiling update

--- midibuilder8/Knob.h
+++ midibuilder8/Knob.h
@@ -4,17 +4,22 @@
 #include <Arduino.h>
 #include "Multiplexer.h"
 
-// Analog Input for Knobs and Faders alike
-class Knob_On_Mux {
-private:
-  Mux& _mux;
-  uint8_t _potPin;  // Mux channel connected to potentiometer
+// Analog Inputs for Knobs and Faders alike
 
+// ========================  KNOB  =========================
+// =========================================================
+// Regular knobs connected directly to MCU's ADC pins
+
+class Knob {
+protected:
+  uint8_t _potPin;  // Mux channel connected to potentiometer
   bool _isEnabled;
   uint8_t _CCNumber;
   uint8_t _channel;
-  uint8_t _minCCValue;  // Minimum CC value
-  uint8_t _maxCCValue;  // Maximum CC value
+  uint16_t _minAnalogValue;  // Maximum analog value from potentiometer readings (0 - 1023) Read in 10 bits
+  uint16_t _maxAnalogValue;  // Maximum analog value from potentiometer readings (0 - 1023) Read in 10 bits
+  uint8_t _minCCValue;       // Minimum CC value
+  uint8_t _maxCCValue;       // Maximum CC value
   uint16_t _potState, _potPState;
   uint8_t _midiState = 0;
   uint8_t _midiPState = 0;
@@ -24,10 +29,10 @@ private:
 
 public:
   // Constructors
-  Knob_On_Mux(Mux& mux, uint8_t potPin, uint8_t CCNumber, uint8_t min, uint8_t max, uint8_t channel, bool isEnabled);
-  Knob_On_Mux(Mux& mux, uint8_t potPin, uint8_t CCNumber, uint8_t min, uint8_t max, uint8_t channel);
-  Knob_On_Mux(Mux& mux, uint8_t potPin, uint8_t CCNumber, uint8_t min, uint8_t max);
-  Knob_On_Mux(Mux& mux, uint8_t potPin, uint8_t CCNumber);
+  Knob(uint8_t potPin, uint8_t CCNumber, uint8_t min, uint8_t max, uint8_t channel, bool isEnabled);
+  Knob(uint8_t potPin, uint8_t CCNumber, uint8_t min, uint8_t max, uint8_t channel);
+  Knob(uint8_t potPin, uint8_t CCNumber, uint8_t min, uint8_t max);
+  Knob(uint8_t potPin, uint8_t CCNumber);
 
   struct MinMax {
     uint8_t min, max;
@@ -39,15 +44,48 @@ public:
 
   // Setters
   void setMIDIChannel(uint8_t channel);
-  void setMin(uint8_t value);
-  void setMax(uint8_t value);
+  void setAnalogMin(uint16_t minAnalogValue);
+  void setAnalogMax(uint16_t maxAnalogValue);
+  void setCCMin(uint8_t CCMinValue);
+  void setCCMax(uint8_t CCMaxValue);
 
   // Methods
+  void enable();
   void disable();
   void readKnob();
   void validateAnalogRead(uint16_t reading);
   void update();
 };
+// =========================================================
+// =========================================================
+
+
+// ====================== KNOB ON MUX ======================
+// =========================================================
+// Modified knob class to read Analog from Multiplexer.
+// Inherits the Knob class
+
+class Knob_On_Mux : public Knob {
+private:
+  Mux& _mux;
+  uint8_t _potPin;  // Mux channel connected to potentiometer
+
+public:
+  // Constructors
+  Knob_On_Mux(Mux& mux, uint8_t potPin, uint8_t CCNumber, uint8_t min, uint8_t max, uint8_t channel, bool isEnabled);
+  Knob_On_Mux(Mux& mux, uint8_t potPin, uint8_t CCNumber, uint8_t min, uint8_t max, uint8_t channel);
+  Knob_On_Mux(Mux& mux, uint8_t potPin, uint8_t CCNumber, uint8_t min, uint8_t max);
+  Knob_On_Mux(Mux& mux, uint8_t potPin, uint8_t CCNumber);
+
+  // Getters
+
+  // Setters
+
+  // Methods
+  void readKnob();
+};
+// =========================================================
+// =========================================================
 
 
 #endif
